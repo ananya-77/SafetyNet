329761eee8a8e0ab47bf98ba591937a7bf0495a7
ananya-77 <108771381+ananya-77@users.noreply.github.com>
2025-04-08 11:13:02 +0530

    Add files via upload

--- models/Report.js
+++ models/Report.js
@@ -1,1 +1,8 @@
+const mongoose = require("mongoose");
 
+const reportSchema = new mongoose.Schema({
+    result: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Report", reportSchema);
